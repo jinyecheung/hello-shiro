3acbd001b73721556377290bf69feec977d3194d
zhangjinpei <zhangjinpei@chalco-steering.com>
2021-01-28 15:40:04 +0800

    modify20210128153946

---
+++ src/main/java/com/example/hello/shiro/HelloShiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShiroApplication.class, args);
+	}
+
+}
