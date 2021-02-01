60015bcea0d13106d29404c40f2edbff685bef85
zhangjinpei <zhangjinpei@chalco-steering.com>
2021-02-01 10:31:21 +0800

    modify,add99,20210201103037

--- src/main/java/com/example/hello/shiro/HelloShiroApplication.java
+++ src/main/java/com/example/hello/shiro/HelloShiroApplication.java
@@ -2,12 +2,27 @@ package com.example.hello.shiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+/**
+ * @author zhangjinpei at 2021/02/01
+ */
 @SpringBootApplication
-public class HelloShiroApplication {
+public class HelloShiroApplication extends WebMvcConfigurationSupport {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloShiroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShiroApplication.class, args);
+    }
 
+	/**
+	 * 这里配置静态资源文件的路径导包都是默认的直接导入就可以
+	 * @param registry object
+	 */
+	@Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+        super.addResourceHandlers(registry);
+    }
 }
